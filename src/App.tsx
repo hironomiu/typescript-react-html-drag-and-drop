482cbfe0b3819c5ffa4ffda340b7922cff1cd09d
hironomiu <hironomiu@gmail.com>
2022-04-13 09:00:22 +0900

    commited

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Main from './components/Main'
 
 const App = () => {
   return (
-    <div>
+    <div className="m-0 overflow-hidden h-[100vh] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <Main />
     </div>
   )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ const Card = ({ card }: { card: Todo }) => {
       id={card.id.toString()}
       key={card.id}
       draggable
-      className="draggable w-[15vw] h-[20vh] bg-green-500 my-2 rounded-xl flex items-center justify-center hover:bg-green-700 hover:cursor-pointer"
+      className="draggable w-[15vw] h-20 bg-green-500 my-2 rounded-xl flex items-center justify-center hover:bg-green-700 hover:cursor-pointer"
     >
       <span>{card.title}</span>
     </div>
