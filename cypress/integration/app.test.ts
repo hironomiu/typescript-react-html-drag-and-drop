bc46e48f60a30eb4c8952ee0f22e3add5ce654b4
hironomiu <hironomiu@gmail.com>
2022-04-17 10:04:19 +0900

    commited

--- cypress/integration/app.test.ts
+++ cypress/integration/app.test.ts
@@ -3,5 +3,7 @@ describe('App', () => {
     cy.visit('/')
     cy.get('[data-testid=card-create-button]').click()
     cy.get('[data-testid=card-1]').trigger('mouseover')
+    cy.get('[data-testid=card-1]').click()
+    cy.get('[data-testid=card-modal-close-button').click()
   })
 })
--- src/components/modal/CardModal.tsx
+++ src/components/modal/CardModal.tsx
@@ -23,7 +23,7 @@ const CardModal = () => {
         <div className="bg-blue-100 rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative">
           <div className="md:flex items-start w-96 h-64">
             <div className="mt-4 md:mt-0 md:mx-6 text-center md:text-left w-screen">
-              <p className="font-bold text-2xl">Your Todo</p>
+              <p className="font-bold text-2xl text-blue-900">Your Todo</p>
               <p className="text-sm text-gray-700 mt-1">
                 <input
                   className="w-full h-8 p-2"
@@ -37,12 +37,20 @@ const CardModal = () => {
                   value={cardModalData.body}
                 />
               </p>
+              {/* TODO: 動的に設定する */}
+              <select className="mt-2 p-2" name="aaaa" id="">
+                <option value="">状況を選択</option>
+                <option value="todo">todo</option>
+                <option value="doing">doing</option>
+                <option value="done">done</option>
+              </select>
             </div>
           </div>
           <div className="text-center md:text-right mt-4 md:flex md:justify-end">
             <button
               onClick={() => dispatch(setIsModalOn(false))}
               className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 hover:bg-blue-400 bg-blue-200 text-blue-700 rounded-lg font-semibold text-sm md:ml-2 md:order-2"
+              data-testid="card-modal-update-button"
             >
               Update
             </button>
@@ -50,6 +58,7 @@ const CardModal = () => {
               onClick={() => dispatch(setIsModalOn(false))}
               className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 hover:bg-gray-400 bg-gray-200 rounded-lg font-semibold text-sm mt-4
           md:mt-0 md:order-1"
+              data-testid="card-modal-close-button"
             >
               Close
             </button>
