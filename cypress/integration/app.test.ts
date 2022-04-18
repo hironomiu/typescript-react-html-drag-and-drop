d16bf674c8e85362df52883e57650e7f6dc23bee
hironomiu <hironomiu@gmail.com>
2022-04-18 18:29:43 +0900

    commited

--- cypress/integration/app.test.ts
+++ cypress/integration/app.test.ts
@@ -2,6 +2,7 @@ describe('App', () => {
   it('Card CreateボタンをクリックしBoardにCardの作成', () => {
     cy.visit('/')
     cy.get('[data-testid=card-create-button]').click()
+    cy.get('[data-testid=card-modal-close-button').click()
     cy.get('[data-testid=card-1]').trigger('mouseover')
     cy.get('[data-testid=card-1]').click()
     cy.get('[data-testid=card-modal-close-button').click()
