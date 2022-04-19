d473c3859aed59b55e2fdef18970e1038799026b
hironomiu <hironomiu@gmail.com>
2022-04-19 10:23:11 +0900

    commited

--- cypress/integration/app.test.ts
+++ cypress/integration/app.test.ts
@@ -4,6 +4,7 @@ describe('App', () => {
     cy.get('[data-testid=card-create-button]').click()
     cy.get('[data-testid=title-input]').type('test title')
     cy.get('[data-testid=body-textarea]').type('test body')
+    cy.get('select').select('todo')
     cy.get('[data-testid=card-modal-close-button').click()
     cy.get('[data-testid=card-1]').trigger('mouseover')
     cy.get('[data-testid=card-1]').click()
