0fb519cbac19a7ea8d15b26686fd35b5a6b8ac4d
hironomiu <hironomiu@gmail.com>
2022-04-20 11:30:26 +0900

    commited

--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { response } from 'msw'
 import { RootState } from '../../app/store'
 import { BoardType, Todo } from '../../types'
 
