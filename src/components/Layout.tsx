ac6a7ac08a518056e96f4b62305d89c53c82dd67
hironomiu <hironomiu@gmail.com>
2022-04-21 12:25:06 +0900

    commited

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router-dom'
+
 const Layout = () => {
+  useEffect(() => {
+    // TODO: reduxにもっていく
+    ;(async () => {
+      const response = await fetch('http://localhost:8888/api/v1/csrf-token')
+      const data = await response.json()
+      console.log(data)
+    })()
+  }, [])
+
   return (
     // Gradient Color Stops https://tailwindcss.com/docs/gradient-color-stops
     <div className="m-0 overflow-hidden h-[100vh] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
