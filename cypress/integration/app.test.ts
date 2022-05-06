06674b61d3624b3a7b322231c5d5575b846696d1
hironomiu <hironomiu@gmail.com>
2022-05-06 18:23:04 +0900

    commited

--- cypress/integration/app.test.ts
+++ cypress/integration/app.test.ts
@@ -1,19 +1,39 @@
 describe('App', () => {
   it('Card CreateボタンをクリックしBoardにCardの作成', () => {
-    cy.visit('/')
-    // cy.location('/signin', { timeout: 10000 })
-    cy.get('[data-testid=signin-button]').click()
-    // cy.get('[data-testid=card-create-button]').click()
+    cy.visit('/signin', { log: true })
+    cy.title().should('eq', 'React App')
+    // eslint-disable-next-line testing-library/await-async-utils
+    cy.wait(100)
+    let _csrf: any
+    cy.getCookie('_csrf', { log: true })
+      .should('exist')
+      .then((c: any) => {
+        _csrf = c
+      })
+
+    cy.get('[data-testid=signin-button]')
+      .click()
+      .then(() => {
+        cy.request({
+          url: '  http://localhost:8888/api/v1/auth/signin',
+          headers: {
+            _csrf: _csrf.value,
+          },
+        })
+      })
+
+    // TODO: _csrfの渡し方
+    cy.get('[data-testid=card-create-button]').click()
     // cy.get('[data-testid=title-input]').type('test title')
     // cy.get('[data-testid=body-textarea]').type('test body')
     // cy.get('select').select('todo')
     // cy.get('[data-testid=card-modal-create-and-update-button').click()
   })
-  it('Cardをマウスオーバー', () => {
-    cy.get('[data-testid=card-1]').trigger('mouseover')
-  })
-  it('Cardをクリックしupdateモーダルの表示', () => {
-    cy.get('[data-testid=card-1]').click()
-    cy.get('[data-testid=card-modal-close-button').click()
-  })
+  // it('Cardをマウスオーバー', () => {
+  //   cy.get('[data-testid=card-1]').trigger('mouseover')
+  // })
+  // it('Cardをクリックしupdateモーダルの表示', () => {
+  //   cy.get('[data-testid=card-1]').click()
+  //   cy.get('[data-testid=card-modal-close-button').click()
+  // })
 })
